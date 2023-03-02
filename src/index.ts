eef7e8b73c80f8245669b2c44f2c0663cd9e37a5
SE-Yukun <yukun@team.anylinks.jp>
2023-03-03 02:04:48 +0900

    add: comment

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import * as Discord from "discord.js";
 import * as DiscordVoice from "@discordjs/voice";
-import * as Youtube from "youtube-stream-url";
+import * as YoutubeStreamUrl from "youtube-stream-url";
 import { Logger } from "tslog";
 import { config } from "dotenv";
 
@@ -11,68 +11,85 @@ process.on("uncaughtException", (err) => console.error(err));
 const cli = new Discord.Client({
   intents: [
     Discord.GatewayIntentBits.DirectMessageReactions,
-    Discord.GatewayIntentBits.DirectMessageTyping,
     Discord.GatewayIntentBits.DirectMessages,
-    Discord.GatewayIntentBits.GuildBans,
     Discord.GatewayIntentBits.GuildEmojisAndStickers,
     Discord.GatewayIntentBits.GuildIntegrations,
     Discord.GatewayIntentBits.GuildInvites,
     Discord.GatewayIntentBits.GuildMembers,
-    Discord.GatewayIntentBits.GuildMessageReactions,
-    Discord.GatewayIntentBits.GuildMessageTyping,
     Discord.GatewayIntentBits.GuildMessages,
-    Discord.GatewayIntentBits.GuildPresences,
-    Discord.GatewayIntentBits.GuildScheduledEvents,
     Discord.GatewayIntentBits.GuildVoiceStates,
-    Discord.GatewayIntentBits.GuildWebhooks,
     Discord.GatewayIntentBits.Guilds,
     Discord.GatewayIntentBits.MessageContent,
   ],
-  partials: [Discord.Partials.Channel],
-});
-cli.on("ready", async () => {
-  console.info("ready");
 });
 
-cli.on("messageCreate", async (msg) => {
-  if (msg.author.bot) return;
-  if (msg.guild == null) return;
-  if (msg.member == null) return;
-  if (msg.content.startsWith("n!")) {
+// prettier-ignore
+const debug = async (message: any) => !!process.env.debug ? console.debug(message) : void
+
+// prettier-ignore
+cli.on("ready", async () => { console.info("ready"); });
+
+
+cli.on("messageCreate", async (message) => {
+  if (
+    message.author.bot ||
+    !message.guild ||
+    !message.member ||
+    !message.member.voice.channel ||
+    !message.member.voice.channel.isVoiceBased()
+  )
+    return;
+
+  // if start "n!" of message content
+  if (message.content.startsWith("n!")) {
+    // resolve options
     let args: string[];
-    [...args] = msg.content
-      .slice(2, msg.content.length)
+    [...args] = message.content
+      .slice(2, message.content.length)
       .split(" ")
       .filter((m) => m != "");
-    if (msg.member.voice.channel && msg.member.voice.channel.isVoiceBased()) {
-      let con = DiscordVoice.joinVoiceChannel({
-        channelId: msg.member.voice.channel.id,
-        guildId: msg.guild.id,
-        adapterCreator: msg.member.voice.channel.guild.voiceAdapterCreator,
-      });
-      let res = await Youtube.getInfo({ url: args[0] });
-      console.debug(res);
-      if (res != false) {
-        let url: string = res.formats.filter(
-          //@ts-ignore
-          (f) => (f.mimeType as string).startsWith("audio/mp4;")
-        )[0].url;
-        console.debug(url);
-        if (
-          res.videoDetails.isLiveContent == true &&
-          Object.keys(res).find((k) => k == "liveData")
-        ) {
-          //@ts-ignore
-          url = res.liveData.data.segments.filter((d) =>
-            (d.streamInf.codecs[0] as string).includes("mp4a")
-          )[0].url;
-        }
-        let player = DiscordVoice.createAudioPlayer();
-        let src = DiscordVoice.createAudioResource(url, { inlineVolume: true });
-        src.volume?.setVolume((1 / 100) * 4);
-        player.play(src);
-        con.subscribe(player);
-      }
+
+    // create connection
+    let connection = DiscordVoice.joinVoiceChannel({
+      channelId: message.member.voice.channel.id,
+      guildId: message.guild.id,
+      adapterCreator: message.member.voice.channel.guild.voiceAdapterCreator,
+    });
+
+    // get youtube stream url
+    let youtubeStreamUrl = await YoutubeStreamUrl.getInfo({ url: args[0] });
+
+    debug(youtubeStreamUrl);
+    if (youtubeStreamUrl != false) {
+      // get stream url
+      let url: string =
+        // make sure its live stream
+        youtubeStreamUrl.videoDetails.isLiveContent == true &&
+        !!Object.keys(youtubeStreamUrl).find((k) => k == "liveData")
+          ? // if its live, get live stream url
+            (youtubeStreamUrl as any).liveData.data.segments.filter((d: any) =>
+              (d.streamInf.codecs[0] as string).includes("mp4a")
+            )[0].url
+          : // if its not live, get archive stream url
+            youtubeStreamUrl.formats.filter((f: any) =>
+              (f.mimeType as string).startsWith("audio/mp4;")
+            )[0].url;
+
+      // create audio player
+      let player = DiscordVoice.createAudioPlayer();
+
+      // create audio resource
+      // prettier-ignore
+      let audioResource = DiscordVoice.createAudioResource(url, { inlineVolume: true });
+
+      // configure audio volume (0.04 / 100)
+      audioResource.volume!.setVolume((1 / 100) * 4);
+
+      // set audio resource to player
+      player.play(audioResource);
+
+      // set audio player to connection
+      connection.subscribe(player);
     }
   }
 });
